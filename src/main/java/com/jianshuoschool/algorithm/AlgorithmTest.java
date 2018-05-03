2ecf53ad7b278f127dbf26b51a5e87b43238c85c
zhen.yu <iszhenyu@yeah.net>
2018-05-03 08:31:31 +0800

    ..

--- src/main/java/com/jianshuoschool/algorithm/AlgorithmTest.java
+++ src/main/java/com/jianshuoschool/algorithm/AlgorithmTest.java
@@ -1,5 +1,7 @@
 package com.jianshuoschool.algorithm;
 
+import com.jianshuoschool.algorithm.sort.MergeSort;
+
 /**
  * @author zhen.yu
  * @since 2018/5/3
@@ -7,6 +9,25 @@ package com.jianshuoschool.algorithm;
 public class AlgorithmTest {
 
     public static void main(String[] args) {
+        int[] arr = new int[]{3, 5, 7, 2, 1, 9};
+        testMergeSort(arr);
+    }
 
+    private static void testMergeSort(int[] arr) {
+        printArr(arr);
+        MergeSort.sort(arr);
+        printArr(arr);
+    }
+
+    private static void printArr(int[] arr) {
+        StringBuilder sb = new StringBuilder("Array::[");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i != (arr.length - 1)) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        System.out.println(sb.toString());
     }
 }
--- src/main/java/com/jianshuoschool/algorithm/sort/MergeSort.java
+++ src/main/java/com/jianshuoschool/algorithm/sort/MergeSort.java
@@ -7,7 +7,49 @@ package com.jianshuoschool.algorithm.sort;
 public class MergeSort {
 
     public static void sort(int[] arr) {
+        int size = arr.length;
+        sub_sort(arr, 0, size - 1);
+    }
 
+    private static void sub_sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int center = left + (right - left) / 2;
+            sub_sort(arr, left, center);
+            sub_sort(arr, center + 1, right);
+            merge(arr, left, center, center + 1, right);
+        }
+    }
+
+    private static void merge(int[] oriArr, int left1, int right1, int left2, int right2) {
+        int[] assistArr = new int[oriArr.length];
+        int pointer1 = left1;
+        int pointer2 = left2;
+        int tmp = left1;
+        while (pointer1 <= right1 && pointer2 <= right2) {
+            if (oriArr[pointer1] < oriArr[pointer2]) {
+                assistArr[tmp] = oriArr[pointer1];
+                pointer1++;
+            } else {
+                assistArr[tmp] = oriArr[pointer2];
+                pointer2++;
+            }
+            tmp++;
+        }
+        while (pointer1 <= right1) {
+            assistArr[tmp] = oriArr[pointer1];
+            tmp++;
+            pointer1++;
+        }
+        while (pointer2 <= right2) {
+            assistArr[tmp] = oriArr[pointer2];
+            tmp++;
+            pointer2++;
+        }
+        tmp = left1;
+        while (tmp <= right2) {
+            oriArr[tmp] = assistArr[tmp];
+            tmp++;
+        }
     }
 
 }
