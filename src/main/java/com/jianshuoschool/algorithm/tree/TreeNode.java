89220c8765bb9bac7f08479570f7eb2b5b176b3a
zhen.yu <iszhenyu@yeah.net>
2018-05-04 20:19:28 +0800

    ..

---
+++ src/main/java/com/jianshuoschool/algorithm/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package com.jianshuoschool.algorithm.tree;
+
+/**
+ * @author zhen.yu
+ * @since 2018/5/4
+ */
+public class TreeNode {
+    public int data;
+    public TreeNode leftChild;
+    public TreeNode rightChild;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
