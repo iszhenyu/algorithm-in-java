9cb08dbe0b9a9795d8a30bace8ee22c3dce2e32a
zhen.yu <iszhenyu@yeah.net>
2018-05-04 11:22:58 +0800

    ..

--- src/main/java/com/jianshuoschool/algorithm/sort/CountiingSort.java
+++ src/main/java/com/jianshuoschool/algorithm/sort/CountiingSort.java
@@ -13,10 +13,15 @@ public class CountiingSort {
      * @param k 输入数组的每个元素在[0, k]之间
      */
     public static void sort(int[] input, int[] output, int k) {
+        // 创建0-k的数组，并初始化为0
         int[] base = new int[k + 1];
+        // 对于input中的每个元素，如果值i出现一次，则相应的base[i]加1
+        // 最终base数组中每个位置的数值大小代表了取该值的元素数量
         for (int i = 0; i < input.length; i++) {
             base[input[i]] += 1;
         }
+        // 因为base是已经排序好了的，所以从索引0开始，将当前索引处的值加到下一个索引的值上
+        // 最后base数组每个元素的值大小，代表了有多少个元素小于等于当前索引的大小
         for (int i = 1; i <= k; i++) {
             base[i] += base[i - 1];
         }
