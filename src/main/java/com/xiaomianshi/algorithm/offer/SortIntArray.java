20abd43920243f84097eac30366a37b61d3689c6
zhen.yu <iszhenyu@yeah.net>
2018-06-04 17:06:45 +0800

    ..

--- src/main/java/com/xiaomianshi/algorithm/offer/SortIntArray.java
+++ src/main/java/com/xiaomianshi/algorithm/offer/SortIntArray.java
@@ -6,7 +6,7 @@ import com.xiaomianshi.algorithm.Common;
  * @author zhen.yu
  * @since 2018/5/11
  *
- * 输入一个整数数组，将数组中所有计数排在数组的前面，偶数排在后面
+ * 输入一个整数数组，将数组中所有奇数排在数组的前面，偶数排在后面
  */
 public class SortIntArray {
 
